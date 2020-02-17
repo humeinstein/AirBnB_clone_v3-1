ffefb7c8c88afaacf0c0c39eb83c68b3f9b46234
Gareth Brickman <977@holbertonschool.com>
2020-02-17 03:11:32 +0000

    [FIX] Get values from attrs of amenities, push into a dict object

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,18 +2,23 @@
    Else deletes id from list
 */
 $(document).ready(function () {
-  const idList = [];
-  if ($('input:checkbox').is(':checked')) {
-    idList.push($(':amenity_id'));
-  } else {
-    for (let i = idList.length - 1; i >= 0; i--) {
-      if (idList[i] === $(':amenity_id')) {
-        idList.splice(i, 1);
-      }
-    }
-  }
-  // Appends the list contents as list elements in amenities
-  for (let i = 0; i < idList.length; i++) {
-    $('#h4').find('.amenities').append('<li> + idList[i] + </li>');
-  }
+  let dataDict = {};
+  console.log("#### SCRIPT LOADED ####");
+  $('input[type="checkbox"]').on('click', function () {
+    console.log("#### CLICKED ####");
+    $('input[type="checkbox"]:checked').each(function () {
+      dataDict[$(this).attr("data-id")] = $(this).attr("data-name");
+      console.log(dataDict);
+    });
+  });
 });
+
+//     for (let i = idList.length - 1; i >= 0; i--) {
+//       if (idList[i] === $('amenity.id')) {
+//         idList.splice(i, 1);
+//       }
+//     }
+//   });
+//   // Appends the list contents as list elements in amenities
+//   $('.amenities').find('h4').append(idList.join(', '));
+// });
